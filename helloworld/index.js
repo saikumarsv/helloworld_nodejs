dc3f8844194be5ddd31fa1c9bf153d2dc49ec1d8
saikumarsv <saikumar.sv@gmail.com>
2025-01-15 00:12:15 +0000

    update

--- helloworld/index.js
+++ helloworld/index.js
@@ -1,7 +1,8 @@
 module.exports = async function (context, req) {
+    context.log("Hello World function processed a request.");
+
     context.res = {
         status: 200,
         body: "Hello, World!"
     };
-    console.log("Hello, World!");
 };
